ed0c72f07c92d496ad912a51cd26caa6c73f90ce
Marat Vyshegorodtsev <marat@salesforce.com>
2017-11-01 16:31:17 -0500

    Version update

--- dist/index.js
+++ dist/index.js
@@ -73,6 +73,13 @@ module.exports =
 
 var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
 
+/**
+ * @author Marat Vyshegorodtsev
+ * @license BSD-3-Clause
+ * For full license text, see LICENSE file in the repo root
+ * or https://opensource.org/licenses/ISC
+ */
+
 var auraComponentConfig = __webpack_require__(1);
 var codeStyleRulesConfig = __webpack_require__(2);
 
