83de6b61d1433e7581f8c63990869db958a07d92
Julien <julien.roche@salesforce.com>
2019-05-22 14:58:38 +0200

    chore: simplification and use at least now node 8

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+  plugins: [],
+  extends: ["airbnb"],
+  rules: {
+    "arrow-parens": 0,
+    "comma-dangle": 0,
+    "func-names": 1,
+    "prefer-arrow-callback": 1,
+    quotes: ["error", "single"]
+  }
+};
