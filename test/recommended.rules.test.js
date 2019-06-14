924f163f25074a3e53f8bc04bcbd5eaaee002db4
Julien <julien.roche@salesforce.com>
2019-06-14 11:13:53 +0200

    feat(test): put a base of recommended rules test

--- test/recommended.rules.test.js
+++ test/recommended.rules.test.js
@@ -9,16 +9,11 @@ describe('Recommended rules - ', () => {
 
     beforeEach(() => {
         jest.mock('@salesforce/eslint-plugin-aura', () => mockEslintAura, { virtual: true });
-        cli = new CLIEngine(Object.assign(
-            {
-                useEslintrc: false,
-                ignore: false
-            },
-            mockEslintAura.configs.recommended,
-            {
-                globals: Object.keys(mockEslintAura.configs.recommended.globals)
-            }
-        ));
+        cli = new CLIEngine({
+            baseConfig: mockEslintAura.configs.recommended,
+            useEslintrc: false,
+            ignore: false
+        });
     });
 
     describe('around Aura', () => {
@@ -36,6 +31,36 @@ describe('Recommended rules - ', () => {
             ]);
     
             expect(eslintReport.results[0].messages.find((msg) => msg.message === 'Invalid Aura API')).not.toBeNull();
+        });
+    });
+
+    describe('around recommended rules (basics)', () => {
+        it('with valid code', () => {
+            const eslintReport = cli.executeOnFiles([
+                `${baseTestFiles}/recommended.js`
+            ]);
+    
+            expect(eslintReport.results[0].errorCount).toEqual(0);
+        });
+
+        it('with invalid code', () => {
+            const eslintReport = cli.executeOnFiles([
+                `${baseTestFiles}/unrecommended.js`
+            ]);
+    
+            const [errorReport] = eslintReport.results;
+            expect(errorReport.errorCount).toEqual(9);
+            expect(errorReport.messages.map((msg) => msg.ruleId)).toEqual([
+                'consistent-return',
+                'no-unused-vars',
+                'space-before-blocks',
+                'curly',
+                'no-magic-numbers',
+                'block-spacing',
+                'brace-style',
+                'no-magic-numbers',
+                'eol-last'
+            ]);
         });
     });
   });
