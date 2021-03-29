44ec121879af37ae9de309a2af9348b069a171cd
Pierre-Marie Dartus <p.dartus@salesforce.com>
2021-03-29 12:08:10 +0200

    feat: simplify recommended rules

--- test/lib/config/recommended.js
+++ test/lib/config/recommended.js
@@ -13,41 +13,50 @@ const eslint = new ESLint({
 
 describe('Recommended config', () => {
   it('reports a parsing error when using an ES6 syntax', async () => {
-    const result = await eslint.lintText('const a = 1;');
+    const [result] = await eslint.lintText('const a = 1;');
 
-    assert.strictEqual(result.length, 1);
+    assert.strictEqual(result.messages.length, 1);
 
-    const parsingError = result[0].messages.find((msg) => msg.ruleId === null);
-    assert.ok(parsingError !== undefined);
+    const parsingError = result.messages[0];
+    assert.ok(parsingError.fatal);
     assert.ok(parsingError.message.match(/^Parsing error:/));
   });
 
   it('reports a linting error when using a web platform API not available on IE11', async () => {
-    const result = await eslint.lintText('fetch("http://example.com");');
+    const [result] = await eslint.lintText('fetch("http://example.com");');
 
-    assert.strictEqual(result.length, 1);
+    assert.strictEqual(result.messages.length, 1);
 
-    const compatError = result[0].messages.find(
-      (msg) => msg.ruleId === 'compat/compat'
-    );
-    assert.ok(compatError !== undefined);
+    const compatError = result.messages[0];
+    assert.strictEqual(compatError.ruleId, 'compat/compat');
     assert.ok(compatError.message.match(/^fetch is not supported in.* IE 11/));
   });
 
-  // it("doesn't report error known Aura globals", async () => {
-  //   const result = await eslint.lintText('$A');
-  //   console.log(result)
-  // });
+  it("doesn't report error for known Aura globals", async () => {
+    const [result] = await eslint.lintText(`
+      $A.enqueueAction();
+      FORCE.location.getCurrentPage();
+    `);
+
+    assert.strictEqual(result.messages.length, 0);
+  });
+
+  it("doesn't report error for Aura libraries", async () => {
+    const [result] = await eslint.lintText(`
+      Promise.resolve();
+      moment();
+    `);
+
+    assert.strictEqual(result.messages.length, 0);
+  });
 
   it('reports a linting error when using an unkown Aura API', async () => {
-    const result = await eslint.lintText('$A.util.fake();');
+    const [result] = await eslint.lintText('$A.util.fake();');
 
-    assert.strictEqual(result.length, 1);
+    assert.strictEqual(result.messages.length, 1);
 
-    const compatError = result[0].messages.find(
-      (msg) => msg.ruleId === '@salesforce/aura/aura-api'
-    );
-    assert.ok(compatError !== undefined);
+    const compatError = result.messages[0];
+    assert.strictEqual(compatError.ruleId, '@salesforce/aura/aura-api');
     assert.strictEqual(compatError.messageId, 'invalidApi');
   });
 });
